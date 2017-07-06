46980fd253ec7b3f0e8a2912b3a185cab1b1c567
DuckSoft <ducksoft@foxmail.com>
2017-07-06 14:21:41 +0800

    cleanup.cmd: 清理对象调整
     - 取消清理.pdf
     - 添加清理.md~文件（.md文件经vim编辑后留下的备份）

--- cleanup.cmd
+++ cleanup.cmd
@@ -3,14 +3,14 @@ color a
 :listing
 cls
 echo Listing file to clean...
-for /R %%s in (*.log *.un~ *.aux *.out *.pdf *.toc *.gz) do ( 
+for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.md~) do ( 
 echo  - %%s
 )
 echo Any key to continue.
 pause
 :cleaning
 echo Now cleaning...
-for /R %%s in (*.log *.un~ *.aux *.out *.pdf *.toc *.gz) do ( 
+for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.md~) do ( 
 del %%s
 )
 echo done.
