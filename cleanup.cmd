cd5411f6a5eca933ba99299d22899e22b3f4f076
DuckSoft <ducksoft@foxmail.com>
2017-07-14 18:28:31 +0800

    cleanup.cmd: 修改清理脚本
    支持Vim编辑器的各种残留文件

--- cleanup.cmd
+++ cleanup.cmd
@@ -3,14 +3,14 @@ color a
 :listing
 cls
 echo Listing file to clean...
-for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.md~) do ( 
+for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.*~) do ( 
 echo  - %%s
 )
 echo Any key to continue.
 pause
 :cleaning
 echo Now cleaning...
-for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.md~) do ( 
+for /R %%s in (*.log *.un~ *.aux *.out *.toc *.gz *.*~) do ( 
 del %%s
 )
 echo done.
